2cc3fb06e84e6ffe28ee43100f7c5ff53390ca9e
HammamiHO <hammamihoussem146@gmail.com>
2024-11-24 19:13:37 +0100

    first commit

---
+++ app/src/main/java/com/example/gestion_des_notes/SignUp.java
@@ -0,0 +1,35 @@
+package com.example.gestion_des_notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUp extends AppCompatActivity {
+    TextView T;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        T=findViewById(R.id.log);
+        T.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(SignUp.this, Prof.class);
+                startActivity(i);
+            }
+        });
+    }
+}
