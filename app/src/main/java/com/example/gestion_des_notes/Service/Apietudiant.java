7c8ccbe0b9e59dd16fb35e9041ed2e18c905e135
mohamed-kasmi <hk7060045@gmail.com>
2024-11-26 00:15:32 +0100

    change

--- app/src/main/java/com/example/gestion_des_notes/Service/Apietudiant.java
+++ app/src/main/java/com/example/gestion_des_notes/Service/Apietudiant.java
@@ -7,6 +7,8 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -15,10 +17,14 @@ import retrofit2.http.Path;
 public interface Apietudiant {
     @GET("/etudiant/getalletud")
     Call<List<Etudiant>> getalletud();
+    @FormUrlEncoded
     @POST("/etudiant/addetudbyadmin")
-    Call<String> addetudbyadmin(@Body Etudiant etudiant);
+    Call<String> addetudbyadmin(@Field("cin") int cin,
+                                @Field("classe") String classe);
+    @FormUrlEncoded
     @POST("/etudiant/login")
-    Call<String> loginetud(@Body Etudiant etudiant);
+    Call<String> loginetud(@Field("email") String email,
+                           @Field("password") String password);
     @PUT("/etudiant/signup/{cin}")
     Call<String> signupetud(@Path("cin") int cin,@Body Etudiant etudiant);
     @DELETE("/etudiant/delete/{cinetud")
--- app/src/main/java/com/example/gestion_des_notes/Service/Apiprof.java
+++ app/src/main/java/com/example/gestion_des_notes/Service/Apiprof.java
@@ -7,6 +7,8 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -17,8 +19,14 @@ public interface Apiprof {
     Call<List<Prof>> getallprof();
     @GET("/prof/cin-by-email")
     Call<Integer> getcinprobyfmail(@Query("email") String email);
+    @FormUrlEncoded
     @POST("/prof/signup")
-    Call<String> addprof(@Body Prof prof);
+    Call<Void> addprof(@Field("cin") int cin,
+                       @Field("nom") String nom,
+                       @Field("prenom") String prenom,
+                       @Field("gener") String gener,
+                       @Field("email") String email,
+                       @Field("password") String password);
     @POST("/prof/login")
     Call<String> loginprof(@Body Prof prof);
     @DELETE("/profdelete/{cinprof/")
