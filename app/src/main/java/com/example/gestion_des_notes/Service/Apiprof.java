2ba2314d33f2a963919342c6bae0633ea3ebc4df
mohamed-kasmi <hk7060045@gmail.com>
2024-11-25 16:22:53 +0100

    creation methods of Apiprofreq

--- app/src/main/java/com/example/gestion_des_notes/Service/Apiprof.java
+++ app/src/main/java/com/example/gestion_des_notes/Service/Apiprof.java
@@ -1,4 +1,19 @@
 package com.example.gestion_des_notes.Service;
 
+import com.example.gestion_des_notes.Models.Prof;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface Apiprof {
+    @GET("/prof/getallprof")
+    Call<List<Prof>> getallprof();
+    @POST("/prof/signup")
+    Call<String> addprof(@Body Prof prof);
+    @POST("/prof/login")
+    Call<String> loginprof(@Body Prof prof);
 }
--- app/src/main/java/com/example/gestion_des_notes/Service/Apiprofreq.java
+++ app/src/main/java/com/example/gestion_des_notes/Service/Apiprofreq.java
@@ -1,4 +1,22 @@
 package com.example.gestion_des_notes.Service;
 
+import com.example.gestion_des_notes.Models.Prof_Req;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface Apiprofreq {
+    @GET("/profreq/getallprofreq")
+    Call<List<Prof_Req>> getProfreq();
+    @POST("/profreq/signuprforeq")
+    Call<String> addprofreq(@Body Prof_Req profReq);
+    @DELETE("/profreq/delete/{cinprof}")
+    Call<String> deleteProfreq(@Path("cinprof") int cinprof);
+
 }
